6162bb982d0c67d42da0302af5873375818d0f2f
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-08-24 19:49:16 +0600

    Create recursion.cpp

---
+++ recursion.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}
+
+int main()
+{
+    optimize();
+    cout<<fact(5)<<endl;
+
+}
